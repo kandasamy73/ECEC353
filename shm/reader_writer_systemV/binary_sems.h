3e0619002f62643b281d3436fc4bd4e0a7958796
Naga Kandasamy <nk78@drexel.edu>
2018-11-24 18:19:04 -0500

    Clean up

--- shm/reader_writer_systemV/binary_sems.h
+++
@@ -1,9 +0,0 @@
-#ifndef BINARY_SEMS_H
-#define BINARY_SEMS_H
-
-int initSemAvailable (int semId, int semNum);
-int initSemInUse (int semId, int semNum);
-int reserveSem (int semId, int semNum);
-int releaseSem (int semId, int semNum);
-
-#endif
